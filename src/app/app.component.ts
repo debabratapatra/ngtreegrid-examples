996d193659e2ce5a339f658e2bd185ce5088aa5a
debabratapatra <debabratapatra12@gmail.com>
2019-05-19 22:33:29 +0530

    Apply selection class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,24 @@ export class AppComponent  {
     onAdd(rec) {
           console.log(rec);
     }
+
+    selected_feature = {
+      basic_tree_grid: true,
+      add_edit_delete: false,
+      cond_row_edit: false,
+      custom_view_component: false,
+      custom_edit_component: false,
+      resolve_row_add: false,
+      multilevel_tree_grid: false,
+    }
+  
+    select(feature) {
+      for (const key in this.selected_feature) {
+        if (this.selected_feature.hasOwnProperty(key)) {        
+          this.selected_feature[key] = false;
+        }
+      }
+  
+      this.selected_feature[feature] = true;
+    }
 }
